e173602edb70101b191edf28f95139b3b343b69a
Ronaldo Faustino <Ronaldo Faustino>
2021-05-09 19:55:47 -0300

    Helth Check

---
+++ src/test/java/br/ce/wcaquino/prod/HealtCheckIT.java
@@ -0,0 +1,29 @@
+package br.ce.wcaquino.prod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class HealtCheckIT {
+	
+	@Test
+	public void healtCheck() throws MalformedURLException {
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.72:4444/wd/hub"), cap);
+		try {
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.navigate().to("http://192.168.1.72:9999/tasks");
+			String version = driver.findElement(By.id("version")).getText();
+			Assert.assertTrue(version.startsWith("build"));
+		} finally {
+			driver.quit();
+		}	
+	}
+}
